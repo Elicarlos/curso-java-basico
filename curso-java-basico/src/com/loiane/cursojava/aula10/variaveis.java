b2b0dde8ca370a1b8e9970913ece0aa6f434eb26
Elicarlos <elicarlosantos_@hotmail.com>
2018-07-06 02:54:37 -0300

    Meu primeiro commit

---
+++ curso-java-basico/src/com/loiane/cursojava/aula10/variaveis.java
@@ -0,0 +1,15 @@
+package com.loiane.cursojava.aula10;
+
+public class variaveis {
+
+	public static void main(String[] args) {
+		
+		int idade = 34;
+		String nome, nomeDoMeuCachorro;
+		nome = "Elicarlos";
+		System.out.println("Idade: " + idade + nome);
+		
+
+	}
+
+}
---
+++ curso-java-basico/src/com/loiane/cursojava/aula11/variaveisInteiras.java
@@ -0,0 +1,18 @@
+package com.loiane.cursojava.aula11;
+
+public class variaveisInteiras {
+
+	public static void main(String[] args) {
+	byte idade01 = 20;
+	short idade02 = 30;
+	int idade03 = 45;
+	long idade04 = 56;
+	
+	System.out.println("Valor da variavel 01: " + idade01);
+	System.out.println("Valor da variavel 02: " + idade02);
+	System.out.println("Valor da variavel 03: " + idade03);
+	System.out.println("Valor da variavel 04: " + idade04);
+
+	}
+
+}
